ac410fb44bad7d4318f41d8538823f8fef5317d8
0xz00n <0xz00n@gmail.com>
2020-04-26 19:20:10 -0500

    Added request to EDSM API for pad size that isn't available through EDDN.

--- EDDNListener.py
+++ EDDNListener.py
@@ -1,43 +1,63 @@
 #!/usr/bin/python3 -u
 
-import sys
 import zmq
 import json
 import zlib
+import requests
 from time import sleep
 
-eddnrelay = 'tcp://eddn.edcd.io:9500'
-eddntimeout = 600000
+class EDDNListener():
+    def __init__(self):
+        self.eddnrelay = 'tcp://eddn.edcd.io:9500'
+        self.eddntimeout = 600000
 
-def main():
-    ctx = zmq.Context()
-    sub = ctx.socket(zmq.SUB)
-    sub.setsockopt(zmq.SUBSCRIBE, b"")
-    sub.setsockopt(zmq.RCVTIMEO, eddntimeout)
-    while True:
-        try:
-            sub.connect(eddnrelay)
-            while True:
-                msg = sub.recv()
-                if msg == False:
-                    sub.disconnect(eddnrelay)
-                    break
-                msg = zlib.decompress(msg)
-                jsonmsg = json.loads(msg)
-                if jsonmsg['$schemaRef'] == 'https://eddn.edcd.io/schemas/commodity/3':
-                    for commodity in jsonmsg['message']['commodities']:
-                        if commodity['name'] == 'lowtemperaturediamond':
-                            print(jsonmsg['message']['stationName'] + ', ' + jsonmsg['message']['systemName'])
-                            print('Sell price: ' + str(commodity['sellPrice']))
-                            print('Demand: ' + str(commodity['demand']))
-                            print('--------')
+    def eddn_parser(self):
+        ctx = zmq.Context()
+        sub = ctx.socket(zmq.SUB)
+        sub.setsockopt(zmq.SUBSCRIBE, b"")
+        sub.setsockopt(zmq.RCVTIMEO, self.eddntimeout)
+        while True:
+            try:
+                sub.connect(self.eddnrelay)
+                while True:
+                    msg = sub.recv()
+                    if msg == False:
+                        sub.disconnect(self.eddnrelay)
+                        break
+                    msg = zlib.decompress(msg)
+                    jsonmsg = json.loads(msg)
+                    if jsonmsg['$schemaRef'] == 'https://eddn.edcd.io/schemas/commodity/3':
+                        for commodity in jsonmsg['message']['commodities']:
+                            if commodity['name'] == 'lowtemperaturediamond':
+                                stationname = jsonmsg['message']['stationName']
+                                systemname = jsonmsg['message']['systemName']
+                                sellprice = str(commodity['sellPrice'])
+                                demand = str(commodity['demand'])
+                                padsize = self.pad_size_check(systemname,stationname)
+                                print(stationname + ', ' + systemname)
+                                print('Sell price: ' + sellprice)
+                                print('Demand: ' + demand)
+                                print('Pad size: ' + padsize)
+                                print('--------')
+                    else:
+                        continue
+            except zmq.ZMQError as e:
+                print('ZMQSocketException: ' + str(e))
+                sub.disconnect(self.eddnrelay)
+                sleep(5)
+            break
+
+    def pad_size_check(self,system,station):
+        r = requests.get('https://www.edsm.net/api-system-v1/stations?systemName=' + system)
+        jsonmsg = json.loads(r.text)
+        for entry in jsonmsg['stations']:
+            if entry['name'] == station:
+                if 'outpost' in entry['type'].lower():
+                    size = 'M'
+                    return size
                 else:
-                    continue
-        except zmq.ZMQError as e:
-            print('ZMQSocketException: ' + str(e))
-            sub.disconnect(eddnrelay)
-            sleep(5)
-        break
+                    size = 'L'
+                    return size
 
-if __name__ == '__main__':
-    main()
+EDDNListener = EDDNListener()
+EDDNListener.eddn_parser()
